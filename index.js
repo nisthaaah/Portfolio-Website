4c664a751ec0fe18717e22a99a418bde74f76ee1
nistha <nisthasharma0799@gmail.com>
2023-12-28 22:36:24 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+        const aboutLink = document.querySelector('a[href="#aboutme"]');
+
+        aboutLink.addEventListener("click", function (event) {
+            event.preventDefault();
+
+            const aboutSection = document.getElementById("aboutme");
+            aboutSection.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const scrollToContinue = document.querySelector('.learnmore');
+
+        scrollToContinue.addEventListener("click", function () {
+            const aboutSection = document.getElementById("aboutme");
+            aboutSection.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+
+    document.addEventListener("DOMContentLoaded", function () {
+        const scrollToContinue = document.querySelector('.learnmore');
+        const homeLink = document.getElementById('homelink');
+
+        scrollToContinue.addEventListener("click", function () {
+            const aboutSection = document.getElementById("aboutme");
+            aboutSection.scrollIntoView({ behavior: "smooth" });
+        });
+
+        homeLink.addEventListener("click", function (event) {
+            event.preventDefault();
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        });
+    });
+
+
