32df61922703d2f653451c4d7c4426f575c770df
nistha <nisthasharma0799@gmail.com>
2024-01-31 20:16:52 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.querySelector('a[href="#aboutme"]');
+
+    aboutLink.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const aboutSection = document.getElementById("aboutme");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToContinue = document.querySelector('.learnmore');
+
+    scrollToContinue.addEventListener("click", function () {
+        const aboutSection = document.getElementById("aboutme");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToContinue = document.querySelector('.learnmore');
+    const homeLink = document.getElementById('homelink');
+
+    scrollToContinue.addEventListener("click", function () {
+        const aboutSection = document.getElementById("aboutme");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+
+    homeLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.querySelector('a[href="#resume"]');
+
+    aboutLink.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const aboutSection = document.getElementById("resume");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.querySelector('a[href="#skills"]');
+
+    aboutLink.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const aboutSection = document.getElementById("skills");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.querySelector('a[href="#projects"]');
+
+    aboutLink.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const aboutSection = document.getElementById("projects");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const aboutLink = document.querySelector('a[href="#contact"]');
+
+    aboutLink.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        const aboutSection = document.getElementById("contact");
+        aboutSection.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
